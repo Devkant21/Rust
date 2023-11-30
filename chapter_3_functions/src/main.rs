01f8a29662fc18692bbb3b3ff0a6553b483e0768
Devkant <devkant21@gmail.com>
2023-12-01 00:00:16 +0530

    🚀 : chapter_3_functions

---
+++ chapter_3_functions/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+
+    // always call the new functions inside the main function; otherwise it won't execute
+
+    first_fn();
+}
+
+// simple function
+
+fn first_fn() {
+    println!("first_fn");
+}

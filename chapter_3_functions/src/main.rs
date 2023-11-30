3d2c246cbd208ba627c2f358ad0ccd7880705eb3
Devkant <devkant21@gmail.com>
2023-12-01 00:22:57 +0530

    🚀 : chapter_3_functions : single_params

--- chapter_3_functions/src/main.rs
+++ chapter_3_functions/src/main.rs
@@ -4,10 +4,18 @@ fn main() {
     // always call the new functions inside the main function; otherwise it won't execute
 
     first_fn();
+
+    single_params(23)
 }
 
 // simple function
 
 fn first_fn() {
     println!("first_fn");
+}
+
+// passing a single parameter
+
+fn single_params(x: i32) {
+    println!("The value of x - {}",x);
 }

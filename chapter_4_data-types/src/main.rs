5fd5b31494462bf970fc0ccffcec54db83106772
Devkant <devkant21@gmail.com>
2023-12-03 23:48:57 +0530

    🚀 : chapter_4_data-types

--- chapter_4_data-types/src/main.rs
+++ chapter_4_data-types/src/main.rs
@@ -1,3 +1,40 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Data types in Rust");
+
+    // Scalar types -> types where we can store single value
+    // Integer, Strings, Boolean, Floating, char
+
+    // Integer -> length (8bit, 16, 32, 64, 128, arch)
+
+
+
+    let no = 2;
+    println!("{}", no);
+
+    // Boolean
+
+    let is_male = true;
+    println!("{}", is_male);
+
+    let char = "abc";
+    println!("{}", char);
+
+    let dec =78.99;
+    println!("{}", dec);
+
+
+
+    // Compound types -> types where we can store multiple data types
+    // Arrays, tuples, dictionary,
+
+    //  tuples
+
+    let tup = (32,45,66.77);
+
+    println!("{:?}", tup);
+
+    println!("{}", tup.1);
+    println!("{}", tup.2);
 }

767c0857b03c7332f072745618331f6e97cb95b6
Devkant <devkant21@gmail.com>
2023-12-04 15:34:20 +0530

    🚀 : chapter_5_if_else

---
+++ chapter_5_if_else/src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    println!("if, if-else");
+
+    first();
+    second();
+
+}
+
+
+fn first() {
+    let no = 10;
+
+    if no < 5 {
+        println!("The condition is true");
+    }
+
+    else {
+        let abc = 4+ 7;
+        println!("{}", abc);
+        println!("The condition is false");
+    }
+}
+
+
+fn second() {
+    let no = 2;
+
+    if no % 4 == 0 {
+        println!("Number is divisible by 4");
+    }
+
+    else if no % 6 == 0 {
+        println!("Number is divisible by 6");
+    }
+    else if no % 8 == 0 {
+        println!("Number is divisible by 8");
+    }
+    else {
+        println!("This no is divisible by anyone");
+    }
+}
